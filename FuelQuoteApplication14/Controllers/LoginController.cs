adf26c98fa41c9e12a0e2096f01092f0d0ca41e8
DugyalaPoojitha <poojitharaodugyala@gmail.com>
2021-07-25 23:58:32 -0500

    partial

---
+++ FuelQuoteApplication14/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FuelQuoteApplication14.Models;
+using System.Data.SqlClient;
+
+namespace FuelQuoteApplication14.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View("LoginView");
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(FuelQuoteApplication14.Models.UserCredential model)
+        {
+            using(FuelQuoteDBEntities1 db=new FuelQuoteDBEntities1())
+            {
+                var userdetails = db.UserCredentials.Where(x => x.Username == model.Username && x.Password == model.Password).FirstOrDefault();
+                Session["userID"] = userdetails.Id;
+                if (userdetails == null)
+                {
+                    model.LoginErrorMessage = "Wrong username or password";
+                    return View("LoginView", model);
+                }
+                else
+                {
+                    Session["userID"] = userdetails.Id;
+                    return RedirectToAction("Home", "Client");
+                }
+                
+            }
+            
+        }
+        public ActionResult RegisterView()
+        {
+            return View("Register");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Login");
+        }
+
+        public ActionResult Save_details(UserCredential user)
+        {
+            //sqlConnection con = new SqlConnection()
+
+
+              Models.FuelQuoteDBEntities1 db = new FuelQuoteDBEntities1();
+            UserCredential u = new UserCredential();
+            u.Username = user.Username;
+            u.Password = user.Password;
+
+
+            
+            var max = db.UserCredentials.OrderByDescending(P => P.Id).FirstOrDefault().Id;
+            
+            u.Id = max + 1;
+
+            Session["userid"] = u.Id;
+
+            db.UserCredentials.Add(u);
+            db.SaveChanges();
+            
+
+            return RedirectToAction("Client_info", "Client");
+
+                }
+    }
+}
---
+++ FuelQuoteApplication14/Models/Getdetails.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuelQuoteApplication14.Models
+{
+    public class Getdetails
+    {
+    }
+}
