653494df6a623329d1adc95bd671658299ed3316
sairithik5 <sairithik5@gmail.com>
2021-08-05 20:28:25 -0500

    pricing module

--- FuelQuoteApplication14/Controllers/QuoteController.cs
+++ FuelQuoteApplication14/Controllers/QuoteController.cs
@@ -18,7 +18,63 @@ namespace FuelQuoteApplication14.Controllers
             var address = c.Address1 + " ," + c.Address2 + ", " + c.City + ", " + c.State + ", " + c.ZipCode;
             Quote q = new Quote();
             q.Address = address;
+           
             return View(q);
+        }
+
+        public ActionResult Quote_display(Models.Quote q)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Models.FuelQuoteDBEntities3 db = new Models.FuelQuoteDBEntities3();
+                var id = (int)System.Web.HttpContext.Current.Session["userID"];
+                Client_Info c = db.Client_Info.Where(x => x.Id == id).FirstOrDefault();
+                var Location_factor = 0.0;
+                var Rate_history_factor = 0.0;
+                var current_price = 1.5;
+                ViewBag.current_price = 1.5;
+                var company_profit_factor = 0.1;
+                var gallons_requested_factor = 0.03;
+
+                if (c.State == "TX")
+                {
+                    Location_factor = 0.02;
+                }
+                else
+                {
+                    Location_factor = 0.04;
+                }
+
+                Models.FuelQuoteDBEntities5 db1 = new FuelQuoteDBEntities5();
+                Quote q1 = new Quote();
+
+                var c1 = db1.Quote.Where(a => a.Id == id).OrderBy(a => a.Delivery_date).Count();
+                if (c1 == 0)
+                {
+                    Rate_history_factor = 0;
+                }
+                else
+                {
+                    Rate_history_factor = 0.01;
+                }
+                if (q.Gallons_requested > 1000)
+                {
+                    gallons_requested_factor = 0.02;
+                }
+                else
+                {
+                    gallons_requested_factor = 0.03;
+                }
+
+                q1.price_per_gallon = current_price + (Location_factor - Rate_history_factor + company_profit_factor + gallons_requested_factor) * current_price;
+                q1.Total_amount = q1.price_per_gallon * q.Gallons_requested;
+                q1.Gallons_requested = q.Gallons_requested;
+                q1.Delivery_date = q.Delivery_date;
+                q1.Address = q.Address;
+                return View(q1);
+            }
+            return View("Generate_quote", q);
         }
         public ActionResult Quote_save(Quote q)
         {
