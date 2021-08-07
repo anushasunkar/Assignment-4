2c93f549b0329b405774b65df3011b875231576b
DugyalaPoojitha <poojitharaodugyala@gmail.com>
2021-08-06 23:52:14 -0500

    unit test

---
+++ FuelQuoteApplication14Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FuelQuoteApplication14.Controllers;
+using FuelQuoteApplication14.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelQuoteApplication14.Controllers.Tests
+{
+    [TestClass()]
+    public class LoginControllerTests
+    {
+        private readonly LoginController _controllerLogin;
+        private readonly ClientController _controllerClient;
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            Models.UserCredential userinfo = new Models.UserCredential()
+            {
+                Username = "Pumba1",
+                Password = "Simba@123"
+            };
+        }
+
+        public void ClientProfileTest()
+        {
+            Models.Client_Info clientinfo = new Models.Client_Info()
+            {
+                Address1 = "Cullen Dr",
+                Address2 = "Apt 4",
+                City = "Houston",
+                State = "TX",
+                ZipCode = 77077
+            };
+        }
+    }
+}
